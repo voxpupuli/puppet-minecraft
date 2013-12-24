d8b425a127431b6589afd06b0d86fd952918b9e4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-12-24 01:22:39 -0800

    Discarding params class

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,53 +4,54 @@
 #
 
 class minecraft(
-  $user
-  $group
-  $install_dir
-  $source
-  $version
-  $bukkit_build
-  $manage_java
-  $heap_size
-  $heap_start
-  $ops
-  $banned_players
-  $banned_ips
-  $white_list_players
-
-  $generator_settings
-  $op_permisison_level
-  $allow_nether
-  $level_name
-  $enable_query
-  $allow_flight
-  $announce_achievments
-  $server_port
-  $level_type
-  $enable_rcon
-  $force_gamemode
-  $level_seed
-  $server_ip
-  $max_build_height
-  $spawn_npcs
-  $white_list
-  $spawn_animals
-  $snooper_enabled
-  $hardcore
-  $online_mode
-  $resource_pack
-  $pvp
-  $difficulty
-  $enable_command_block
-  $gamemode
-  $player_idle_timeout
-  $max_players
-  $spawn_monsters
-  $gen_structures
-  $view_distance
-  $spawn_protection
-  $motd
-) inherits minecraft::params {
+  $user                 => 'minecraft', # The user account for the Minecraft service,
+  $group                => 'minecraft', # The user group for the Minecraft service,
+  $install_dir          => '/opt/minecraft',
+  $source               => 'vanilla', # 'vanilla', 'bukkit', or direct source for wget,
+  $version              => '1.7.4', # Minecraft version to download,
+  $bukkit_build         => '', # '' (recommended), 'beta', or 'dev' build,
+  $manage_java          => true, # Manage jre package,
+  $heap_size            => '1024', # The maximum Java heap size in MB,
+  $heap_start           => '256', # The initial Java heap size in MB,
+  $ops                  => [], # Must be arrays for template,
+  $banned_players       => [],
+  $banned_ips           => [],
+  $white_list_players   => [],
+,
+  # The following are server.properties attributes, see,
+  # http://minecraft.gamepedia.com/Server.properties for information,
+  $generator_settings   => '',
+  $op_permisison_level  => '4',
+  $allow_nether         => 'true',
+  $level_name           => 'world',
+  $enable_query         => 'false',
+  $allow_flight         => 'false',
+  $announce_achievments => 'true',
+  $server_port          => '25565',
+  $level_type           => 'DEFAULT',
+  $enable_rcon          => 'false',
+  $force_gamemode       => 'false',
+  $level_seed           => '',
+  $server_ip            => '',
+  $max_build_height     => '256',
+  $spawn_npcs           => 'true',
+  $white_list           => 'false',
+  $spawn_animals        => 'true',
+  $snooper_enabled      => 'true',
+  $hardcore             => 'false',
+  $online_mode          => 'true',
+  $resource_pack        => '',
+  $pvp                  => 'true',
+  $difficulty           => '1',
+  $enable_command_block => 'false',
+  $gamemode             => '0',
+  $player_idle_timeout  => '0',
+  $max_players          => '20',
+  $spawn_monsters       => 'true',
+  $gen_structures       => 'true',
+  $view_distance        => '10',
+  $spawn_protection     => '16',
+  $motd                 => 'A Minecraft Server') {
 
   class { 'minecraft::packages':
     before => Service['minecraft']
