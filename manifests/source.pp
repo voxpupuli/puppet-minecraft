df6e37215c164c7f5780fa409185ae6fa51d34d2
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-16 13:47:02 -0700

    Fixing snapshot regex

--- manifests/source.pp
+++ manifests/source.pp
@@ -3,10 +3,11 @@ class minecraft::source {
   $jar_name = 'minecraft_server'
 
   case $minecraft::source {
-    /^(\d+)\.(\d+)\.(\d+)$/, # Matches Semantic Versioning for vanilla Minecraft, see http://semver.org/
-    /^(\d+{2})w(\d+{2})[a-z]$/: { # Matches current versioning scheme for vanilla Minecraft snapshots, uses the same download source url
+    /^(\d+)\.(\d+)\.(\d+)$/,    # Matches Semantic Versioning for vanilla Minecraft, see http://semver.org/
+    /^(\d{2})w(\d{2})[a-z]$/: { # Matches current versioning scheme for vanilla Minecraft snapshots, uses the same download source URL
       $download = "https://s3.amazonaws.com/Minecraft.Download/versions/${minecraft::source}/minecraft_server.${minecraft::source}.jar"
     }
+    # Downloads latest type of Bukkit server
     'recommended', 'rb', 'stable': {
       $download = 'http://dl.bukkit.org/latest-rb/craftbukkit.jar'
     }
