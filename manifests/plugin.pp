36537b3c434f93f49acb700e641c13bfb702a5d2
Tim Meusel <tim@bastelfreak.de>
2016-06-05 16:57:09 +0200

    puppet-lint: fix trailing_whitespace

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -3,7 +3,7 @@ define minecraft::plugin($plugin_name = $title, $source) {
   if $plugin_name =~ /^.*\.jar$/ {
     fail("minecraft plugin name ${plugin_name} must not end in '.jar'")
   }
-  
+
   wget::fetch { $plugin_name:
     source      => $source,
     destination => "${minecraft::install_dir}/plugins/${plugin_name}.jar",
