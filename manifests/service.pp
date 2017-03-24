a5a21641a6ffa500c350b509776a5f7f5043a12f
Steve Traylen <steve.traylen@cern.ch>
2017-03-24 23:53:46 +0100

    Use a systemd unit on supported systemd
    Introduction of a systemd unit to manage
    minecraft service.
    
    Makes use of the `mcrcon` utility to shutdown the service.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,25 @@
 class minecraft::service {
 
-  file { 'minecraft_init':
-    ensure  => present,
-    path    => '/etc/init.d/minecraft',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0744',
-    content => template('minecraft/minecraft_init.erb'),
+
+  if $facts['systemd'] {
+    ::systemd::unit_file { 'minecraft.service':
+      content => template('minecraft/minecraft.service.erb'),
+      notify  => Service['minecraft'],
+    }
+  } else {
+    file { 'minecraft_init':
+      ensure  => present,
+      path    => '/etc/init.d/minecraft',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0744',
+      content => template('minecraft/minecraft_init.erb'),
+      notify  => Service['minecraft'],
+    }
   }
 
   service { 'minecraft':
     ensure    => running,
     enable    => $minecraft::autostart,
-    subscribe => File['minecraft_init' ],
   }
 }
